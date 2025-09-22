ddcca0ed233e2309b20df0c2f20dffcf9da46d6d
Aditya-MC <adityamc364@gmail.com>
2025-09-22 19:50:16 +0530

    Delete backend directory

--- backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const commentSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  videoId: { type: mongoose.Schema.Types.ObjectId, ref: "Video", required: true },
-  text: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.model("Comment", commentSchema);
